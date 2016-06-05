125388ccb327065f60b251c1bac0e08e1595754b
cxyliuyu <cxyliuyu@sina.com>
2016-06-05 10:29:22 +0800

    。。

--- 1/Application/CaiApi/Controller/UserController.class.php
+++ 1/Application/CaiApi/Controller/UserController.class.php
@@ -1,19 +1,20 @@
 <?php
 namespace CaiApi\Controller;
 use Think\Controller;
-//header("Content-type: text/javascript; charset=utf-8");
 class UserController extends Controller{
 	private $userLogic;
 	
 	function __construct(){
         $this->userLogic = D('User','Logic');        
     }
+
     function login(){
     	$userName = I("request.username");
     	$password = I("request.password");
     	$value = I("request.value");
     	$this->userLogic->login($userName,$password,$value);
     }
+
     function index(){
     	echo "index";
     }
